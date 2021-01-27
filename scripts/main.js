dbb6f7a4b2cace0c2261b7dc91a117608ceb79f6
nicoletatum <nicoletatum@outlook.com>
2021-01-27 00:25:27 -0600

    officer selection is showing. not working tho

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,12 @@ import { getOfficers } from "./officers/OfficerProvider.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { getCriminals } from "./criminals/CriminalProvider.js"
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
+import { OfficerSelect } from "./officers/OfficerSelect.js"
 
 OfficerList()
 getOfficers()
 CriminalList()
 getCriminals()
 ConvictionSelect()
-
+OfficerSelect()
 
--- scripts/officers/OfficerSelect.js
+++ scripts/officers/OfficerSelect.js
@@ -1,20 +1,43 @@
 import { useOfficers, getOfficers } from "./OfficerProvider.js"
 
-const contentTarget = document.querySelector(".filters__crime")
+const contentTarget = document.querySelector(".filters__officer")
+const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("change", changeEvent => {
     if (changeEvent.target.id === "officerSelect") {
-        // Get the name of the selected officer
+        // Get thfe name of the selected officer
         const selectedOfficer = changeEvent.target.value
-
         // Define a custom event
-        const customEvent = new CustomEvent("officerSelected", {
+        const selectOfficer = new CustomEvent("officerSelected", {
             detail: {
                 officer: selectedOfficer
             }
         })
 
         // Dispatch event to event hub
-        eventHub.dispatchEvent(customEvent)
+        eventHub.dispatchEvent(selectOfficer)
     }
 })
+
+const render = officerCollection => {
+    contentTarget.innerHTML = `
+        <select class="dropdown" id="officerSelect">
+            <option value="0">Please select an officer...</option>
+            ${
+                officerCollection.map( 
+                    officerObject => {
+                        const officerArray = officerObject.name
+                        return `<option>${officerArray}</option>`
+                })
+                }
+        </select>
+    `
+}
+
+export const OfficerSelect = () => {
+    getOfficers()
+        .then(() => {
+            const officers = useOfficers()
+            render(officers)
+        })
+}
