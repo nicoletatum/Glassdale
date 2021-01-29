6c95eea4aa19a13a08f44f8dd6e1ab13f41a1417
nicoletatum <nicoletatum@outlook.com>
2021-01-29 16:16:06 -0600

    fixed show notes.more css

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -2,11 +2,10 @@
 export const NoteHTMLConverter = (noteObject) => {
     return `
         <section class="note">
-            <div class="note__text">${noteObject.text}</div>
-            <div class="note__suspect">Title: ${noteObject.suspect}</div>
-            <div class="note__author">Author: ${noteObject.author}</div>
-            <div class="note__date">Date: ${new Date(noteObject.date).toLocaleDateString('en-US')}</div>
-            <div class="note__date">Intuition: ${noteObject.intuition}</div>
+            <div class="noteText">${noteObject.text}</div>
+            <div class="noteSuspect">Title: ${noteObject.suspect}</div>
+            <div class="noteAuthor">Author: ${noteObject.author}</div>
+            <div class="noteDate">Date: ${new Date(noteObject.date).toLocaleDateString('en-US')}</div>
         </section>
     `
     }
